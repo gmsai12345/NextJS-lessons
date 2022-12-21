6853c223b5271afe8aa2d0829125bc9c87c28643
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-21 09:32:53 +0530

    Add files via upload

---
+++ nextjs-lessons 2/pages/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+// (i) file based routing
+// routing in react is really hard and tedious therefore in
+// next js it solves the complexity
+//(ii) pre rendering in nectjs
+// it creates static html webpage in advance
+// therefore better production and search engine optiimizstion
+// (iii) API routes
+// apis with nextjs fronend code can be created in react app
+// and can be called in react
+// (iv)  support css modules which are out of the box
+// (v) Authentication: supports multiple authentication patterns
+// (vi) dev and product build system: optimized production
+// focuses more on the code
+export default function IndexPage() {
+  return (
+    <div>
+      Hello World.{" "}
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+}
