3aebf5c698b82f40cecdcff98e5c003ff6b2aa21
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-12-21 12:32:18 +0530

    Update index.js

--- nextjs-lessons 2/pages/index.js
+++ nextjs-lessons 2/pages/index.js
@@ -15,7 +15,7 @@ import Link from "next/link";
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{" "}
+      Hello World!.{" "}
       <Link href="/about">
         <a>About</a>
       </Link>
